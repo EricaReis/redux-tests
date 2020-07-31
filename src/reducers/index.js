c6f67b7af6c931673dff9a16417c909942b7c4ca
EricaReis <ericareis67@outlook.com>
2020-07-31 14:45:29 -0300

    51

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
-import todos from './todos';
+import todos from './todos'; 
+import store from '../store';
+
+//51min https://www.youtube.com/watch?v=69e1MoUWE1g
 
 export default combineReducers({
     todos,
