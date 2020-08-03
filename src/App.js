a62757a3094e60bec49aee05a2703f1f6a5c2dfc
EricaReis <ericareis67@outlook.com>
2020-08-03 09:17:38 -0300

    add Counter

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import TodoList from './TodoList';
+import Counter from './Counter';
 
 class App extends Component{
   render(){
     return(
       <>
       <Provider store={store}>
-        <TodoList />
+        <div>
+          <TodoList />
+          <Counter />
+        </div>
       </Provider>
       </>
     );
